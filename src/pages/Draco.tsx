399ec6abc7742ebef5bc192d41f3bf2def5e120f
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-06-26 12:07:04 -0500

    clip carousel implemented

--- src/pages/Draco.tsx
+++ src/pages/Draco.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import dracoClip01 from '../assets/videos/DracoClip01.mp4';
 import dracoClip02 from '../assets/videos/DracoClip02.mp4';
+import { Clip } from '../components/VideoData';
 
 const Draco: React.FC = () => {
   const backgroundColor = '#ae51edf1';
+  const videoRefs = useRef<HTMLVideoElement[]>([]);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const videoClips = [dracoClip01, dracoClip02];
+
+  useEffect(() => {
+    const handleEnded = () => {
+      setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoClips.length);
+    };
+
+    const currentVideoRef = videoRefs.current[currentVideoIndex];
+    currentVideoRef.addEventListener('ended', handleEnded);
+
+    return () => {
+      currentVideoRef.removeEventListener('ended', handleEnded);
+    };
+  }, [currentVideoIndex, videoClips.length]);
   return (
     <div>
       <div className='page-background' style={{backgroundColor}}>
@@ -12,15 +29,16 @@ const Draco: React.FC = () => {
       <h1>draco</h1>
       <p>dracoddracodrqacodraco</p>
 
-      
-
-
-      <video width="640" height="360" muted={true} autoPlay={true} loop={true}>
-          <source src={dracoClip01} type='video/mp4'/>
-          Your browser does not support the video tag.
-      </video>
-      <video width="640" height="360" muted={true} autoPlay={true} loop={true}>
-          <source src={dracoClip02} type='video/mp4'/>
+      <video ref={(el) => el && (videoRefs.current[currentVideoIndex] = el)}
+      width="640"
+      height="360"
+      muted={true}
+      autoPlay={true}
+      loop={false}
+      controls={false}
+      key={currentVideoIndex}
+      >
+          <source src={videoClips[currentVideoIndex]} type='video/mp4'/>
           Your browser does not support the video tag.
       </video>
        </div></div>
