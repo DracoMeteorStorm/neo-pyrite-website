02a2828226c8ff2532082cf318d04f3eeacb47ba
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-07-05 15:58:09 -0500

    deleted clutter

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/pages/index.tsx',
+  entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
