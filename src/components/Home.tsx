16fcfeca023c787082d4b8379fe2f6556e391bb4
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-07-13 12:57:25 -0500

    home page adjustments

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,24 +11,26 @@ const Home: React.FC = () => {
     <div className='content-container'>
     
     <div className='container-row'>
-      <div className='container-col-left'>
+      <div className='container-col-left'
+      style={{width:"60%"}}>
         <div className='component-container'
       style={{backgroundColor:pageColor}}>
         fools gold baybeeeeee.
         </div>
         <div className='component-container'
-      style={{backgroundColor:pageColor}}>
-        <TwitterEmbed></TwitterEmbed>
+      style={{backgroundColor:pageColor,height:"32vw"}}>
+        <h1 style={{fontSize:"50px"}}>Latest Video</h1>
+<iframe width={"100%"} height={"100%"} style={{marginTop:"10px"}} src="https://www.youtube.com/embed/BDIRQZt4kgw?si=CfOZiofrb-3XaBtO" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
 
       </div>
 
-      <div className='container-col-right'>
+      <div className='container-col-right'
+      style={{width:"40%"}}>
       <div className='component-container'
-      style={{backgroundColor:pageColor,height:"32vw"}}>
-        <h1 style={{fontSize:"50px"}}>Latest Video</h1>
-<iframe width={"100%"} height={"100%"} style={{marginTop:"10px"}} src="https://www.youtube.com/embed/BDIRQZt4kgw?si=CfOZiofrb-3XaBtO" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-      </div>
+      style={{backgroundColor:pageColor}}>
+        
+      <TwitterEmbed></TwitterEmbed></div>
 
       </div>
 
